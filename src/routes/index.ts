4f5050a15459eba36d5e679636499eceefcafb2b
KEvin <kevinklgvg@gmail.com>
2026-03-08 02:14:51 -0300

    create route GET categorys, return all for userID

--- src/routes/index.ts
+++ src/routes/index.ts
@@ -1,12 +1,15 @@
 // Global
 import { Router } from 'express';
+import { authMiddleware } from '@/middlewares/auth';
 
 //Routes 
 import userRoutes from './userRoutes';
+import categoryRoutes from './categoryRoutes'
 
 const router = Router();
 
 // Rotas 
 router.use('/users', userRoutes);
+router.use('/categorys', categoryRoutes);
 
 export default router;
